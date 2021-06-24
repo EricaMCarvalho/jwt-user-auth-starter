8cba9f1cde1857a1cea522ba9f96d0b623384b9d
Erica Carvalho <ericamcar28@gmail.com>
2021-06-24 21:18:37 +0100

    connect database

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 
+const connectDB = require('./config/database');
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
-app.get('/', (req, res) => res.send('API is running...'))
+app.get('/', (req, res) => res.send('The API is running...'));
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`.cyan.inverse));
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port ${PORT}`.cyan.inverse
+  )
+);
